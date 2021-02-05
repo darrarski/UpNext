eea5cb4847c1f240c0c2d462586a6083038e9a79
Dariusz Rybicki <darrarski@gmail.com>
2021-02-05 17:29:18 +0100

    Refactor snapshot helpers in Testing package

--- Testing/Sources/SwiftUISnapshots_macOS.swift
+++ Testing/Sources/SwiftUISnapshots_macOS.swift
@@ -1,48 +1,7 @@
+#if os(macOS)
+import AppKit
 import SnapshotTesting
 import SwiftUI
-
-#if os(iOS)
-import UIKit
-import XCTest
-
-public func assertSnapshots<View: SwiftUI.View>(
-  matching value: View,
-  as strategies: [String: Snapshotting<View, UIImage>],
-  named name: String? = nil,
-  record recording: Bool = false,
-  file: StaticString = #file,
-  testName: String = #function,
-  line: UInt = #line
-) {
-  guard UIApplication.shared.windows.first?.safeAreaInsets.bottom == .zero
-  else {
-    XCTFail(
-      ["SwiftUI snapshot tests must be run on a simulator of a device ",
-       "without a notch. For example: iPhone 8 or iPhone SE ",
-       "(1st and 2nd generation) simulators."]
-        .joined(),
-      file: file,
-      line: line
-    )
-    return
-  }
-
-  let namedStrategies = strategies.reduce(into: [:]) { result, strategy in
-    result[[name, strategy.key].compactMap { $0 }.joined(separator: "-")] = strategy.value
-  }
-
-  SnapshotTesting.assertSnapshots(
-    matching: value,
-    as: namedStrategies,
-    record: recording,
-    file: file,
-    testName: testName,
-    line: line
-  )
-}
-
-#elseif os(macOS)
-import AppKit
 
 public func assertSnapshots<View: SwiftUI.View>(
   matching view: View,
